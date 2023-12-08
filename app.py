909989732ba392506cd3db4f5a77c53a44febfca
Paraskevas Paraskevas <paraskevasp@gmail.com>
2023-12-08 19:28:45 +0000

    Change API to reflect new Open AI API version in order to avoid January 4th 2024 decommissioning of old APIs.

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-
 import openai
 import os
-
+from openai import OpenAI
 from exceptions.GPTException import GPTException
 from models.query_model import QueryModel
 from typing import Optional
@@ -13,10 +12,12 @@ from pathlib import Path
 import logging
 
 app = FastAPI()
+client = OpenAI(api_key=os.getenv("OPEN_AI_KEY"))
 
 # logging
 log = logging.getLogger("uvicorn")
-openai.api_key = os.getenv("OPEN_AI_KEY")
+
+
 
 templates_directory = Path(__file__).parent / "templates"
 templates = Jinja2Templates(directory=templates_directory)
@@ -36,61 +37,101 @@ async def index(request: Request):
         log.exception("An HTTPException occurred: %s", exc.detail)
         return http_exception_handler(exc)
 
+# This was the route with old completions API
+# @app.post("/ask_gpt4/")
+# async def ask_gpt4(query_params: QueryModel, model: Optional[str] = "text-davinci-003") -> dict:
+#     """
+#     Post Route receive a query and return a response with OpenAIAPI for chat GPT
+#     :param query_params: User input in the form of questions to chat GPT
+#     :param model: type of model as per OpenAIAPI specifications.
+#     :return: Json in the form of a JSONResponse FastAPi instance
+#     """
+#     try:
+#         # Call the OpenAI API
+#         # The model used is not gpt4 to use gpt4 as a model a different API has to be used and
+#         # in specific the openai.ChatCompletion.create(.....)
+#         response = openai.Completion.create(
+#             engine=model,
+#             prompt=query_params.user_input,
+#             max_tokens=1000,
+#             n=1,
+#             stop=None,
+#             temperature=0.5,
+#         )
+#
+#         if len(response.choices) > 0 and hasattr(response.choices[0], "text"):
+#             answer = response.choices[0].text.strip()
+#             return {"response": answer}
+#         else:
+#             error_msg = "ChatGPT response does not contain text attribute."
+#             log.error(error_msg)
+#             raise GPTException(error_msg)
+#
+#             # return {"error": "ChatGPT response does not contain text attribute."}
+#     except GPTException as e:
+#         raise e
+#     except Exception as e:
+#         log.error(f"Exception occurred: {str(e)}")
+#         if not query_params.user_input:  # Empty user_input case
+#             raise GPTException("Empty user_input", status_code=400)
+#         else:
+#             raise GPTException(str(e))
+#     # except Exception as e:
+#     #     return {"error": str(e)}
+#
+#     # Exception handling
+
 
 @app.post("/ask_gpt4/")
-async def ask_gpt4(query_params: QueryModel, model: Optional[str] = "text-davinci-003") -> dict:
+async def ask_gpt4(query_params: QueryModel) -> JSONResponse:
     """
-    Post Route receive a query and return a response with OpenAIAPI for chat GPT
-    :param query_params: User input in the form of questions to chat GPT
-    :param model: type of model as per OpenAIAPI specifications.
-    :return: Json in the form of a JSONResponse FastAPi instance
+    Endpoint to receive a query and return a response using OpenAI's Chat Completions API
+    :param query_params: User input
+    :return: JSONResponse containing the response
     """
     try:
-        # Call the OpenAI API
-        # The model used is not gpt4 to use gpt4 as a model a different API has to be used and
-        # in specific the openai.ChatCompletion.create(.....)
-        response = openai.Completion.create(
-            engine=model,
-            prompt=query_params.user_input,
-            max_tokens=1000,
-            n=1,
-            stop=None,
-            temperature=0.5,
+        # Using the Chat Completions API
+        response = client.chat.completions.create(
+            model=query_params.model,
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": query_params.user_input}
+            ]
         )
 
-        if len(response.choices) > 0 and hasattr(response.choices[0], "text"):
-            answer = response.choices[0].text.strip()
-            return {"response": answer}
+        # Extracting the response
+        if response.choices and len(response.choices) > 0 and response.choices[0].message:
+            return JSONResponse(content={"response": response.choices[0].message.content})
         else:
-            error_msg = "ChatGPT response does not contain text attribute."
-            log.error(error_msg)
-            raise GPTException(error_msg)
-
-            # return {"error": "ChatGPT response does not contain text attribute."}
-    except GPTException as e:
-        raise e
+            raise HTTPException(status_code=500, detail="No response from the model.")
     except Exception as e:
         log.error(f"Exception occurred: {str(e)}")
-        if not query_params.user_input:  # Empty user_input case
-            raise GPTException("Empty user_input", status_code=400)
-        else:
-            raise GPTException(str(e))
-    # except Exception as e:
-    #     return {"error": str(e)}
+        raise HTTPException(status_code=500, detail=str(e))
 
-    # Exception handling
-
-
+# @app.exception_handler(HTTPException)
+# async def http_exception_handler(exc: HTTPException) -> dict:
+#     """
+#         Function for exception handling. for old gpt
+#         :param exc: the relevant exception raised
+#         :return: dictionary, key , value a pair of status code and error detail.
+#         """
+#
+#     log.debug("Calling http_exception_handler")
+#     return {"detail": exc.detail, "status_code": exc.status_code}
 @app.exception_handler(HTTPException)
-async def http_exception_handler(exc: HTTPException) -> dict:
+async def http_exception_handler(request: Request, exc: HTTPException) -> JSONResponse:
     """
-        Function for exception handling.
-        :param exc: the relevant exception raised
-        :return: dictionary, key , value a pair of status code and error detail.
-        """
-
+    Function for HTTP exception handling.
+    :param request: the request that caused the exception
+    :param exc: the relevant exception raised
+    :return: JSONResponse with error detail and status code.
+    """
     log.debug("Calling http_exception_handler")
-    return {"detail": exc.detail, "status_code": exc.status_code}
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"detail": exc.detail}
+    )
+
 
 
 @app.exception_handler(GPTException)
--- models/query_model.py
+++ models/query_model.py
@@ -2,4 +2,5 @@ from pydantic import BaseModel, Field
 
 
 class QueryModel(BaseModel):
-    user_input: str = Field(min_length=1, max_length=1000)
+    user_input: str = Field(min_length=1, max_length=2000)
+    model: str = "gpt-4"  # Default model

ee3e0401cbbf6f90b4e0f4a5c907ffcf5264abbc
Paraskevas Paraskevas <paraskevasp@gmail.com>
2024-01-30 20:31:33 +0000

    latest commit

--- run.py
+++ run.py
@@ -1,11 +1,6 @@
 import uvicorn
 
 if __name__ == "__main__":
-    # uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True,ssl_keyfile="key.pem",
-    #             ssl_certfile="cert.pem")
-
     config = uvicorn.Config("app:app", host="127.0.0.1", port=8000, reload=False)
     server = uvicorn.Server(config)
     server.run()
-
-# --ssl-keyfile=newkey.pem --ssl-certfile=cert.pem
